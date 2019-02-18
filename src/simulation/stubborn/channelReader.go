60594a3fe3691b3698aab19ac2de6e0ea5f460f5
Ricardo Pereira <ricardo.agp.914@gmail.com>
2019-02-18 22:45:25 +0000

    Fix log of exchanged messages

--- src/simulation/stubborn/channelReader.go
+++ src/simulation/stubborn/channelReader.go
@@ -3,18 +3,22 @@ package stubborn
 // SReceive is the method that receives the messages through the channel
 func (channel *Channel) SReceive() (pack *Package) {
 	for {
-		pack := <-channel.InputBuffer
-		channel.Metrics.incrementMessagesReceived(pack.ID)
-		if pack.Ack {
-			oldPack := channel.OutputBuffer.GetElement(pack.ID)
-			oldPack.Arrived = true
-			channel.OutputBuffer.InsertElement(pack.ID, oldPack)
-		} else if pack.Suspicious {
+        pack := <-channel.InputBuffer
+
+        if pack.Suspicious {
             channel.suspected(pack.SuspiciousID)
         } else {
-			go channel.ack(pack.ID)
-			return pack
-		}
+            channel.Metrics.incrementMessagesReceived(pack.ID)
+
+            if pack.Ack {
+                oldPack := channel.OutputBuffer.GetElement(pack.ID)
+                oldPack.Arrived = true
+                channel.OutputBuffer.InsertElement(pack.ID, oldPack)
+            } else {
+                go channel.ack(pack.ID)
+                return pack
+            }
+        }
 	}
 }
 
