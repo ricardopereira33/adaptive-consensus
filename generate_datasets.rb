f8ca843ffb6733d065aeda42bbe59817fef54a6a
Ricardo Pereira <ricardo.agp.914@gmail.com>
2019-05-04 22:51:59 +0100

    Update dataset generator

--- generate_datasets.rb
+++ generate_datasets.rb
@@ -1,39 +1,29 @@
 require 'timeout'
 
-initial_default_delta       = 0.5
-initial_max_tries           = [3, 5, 10]
-initial_percentage_miss     = 0
-initial_percentage_faults   = 0
-initial_probability_to_fail = 0
-initial_bandwidth           = 20
-initial_latency             = 25
+def run(command)
+  pid = Process.spawn(command)
+  begin
 
-nodes     = 10
-mutations = ['early', 'ring', 'centralized', 'gossip']
+    Timeout.timeout(300) do
+      Process.wait(pid)
+    end
+  rescue Timeout::Error
+    puts "Timeout - #{mutation}"
+    Process.kill('TERM', pid)
+  end
+end
 
-puts 'Start!'
-
-nodes.step(100, 30) do |nodes_number|
-  initial_default_delta.step(2, 0.5) do |default_delta|
-    initial_max_tries.each do |max_tries|
-      initial_percentage_miss.step(8, 4) do |percentage_miss|
-        initial_percentage_faults.step(20, 5) do |percentage_fault|
-          initial_probability_to_fail.step(10, 5) do |probability_to_fail|
-            initial_bandwidth.step(200, 40) do |bandwidth|
-              initial_latency.step(250, 50) do |latency|
-                mutations.each do |mutation|
-                  if not (percentage_fault == 0 and probability_to_fail > 0)
-                    command = "./bin/simulation #{mutation} #{nodes_number} #{default_delta} #{max_tries} #{percentage_miss} #{latency} #{bandwidth} #{percentage_fault} #{probability_to_fail} false"
-
-                    pid = Process.spawn(command)
-                    begin
-                      Timeout.timeout(300) do
-                        Process.wait(pid)
-                      end
-                    rescue Timeout::Error
-                      puts "Timeout - #{mutation}"
-                      Process.kill('TERM', pid)
-                    end
+def execute(mutation, nodes, default_delta, max_tries, percentage_miss, percentage_faults, probability_to_fail, bandwidth, latency)
+  nodes.each do |nodes_number|
+    default_delta.each do |delta|
+      max_tries.each do |max|
+        percentage_miss.each do |percentage|
+          percentage_faults.each do |percentage_fault|
+            probability_to_fail.each do |probability|
+              bandwidth.each do |bandw|
+                latency.each do |lat|
+                  if !(percentage_fault == 0 and probability_to_fail > 0)
+                    run("./bin/simulation #{mutation} #{nodes_number} #{delta} #{max} #{percentage} #{lat} #{bandw} #{percentage_fault} #{probability} false")
                   end
                 end
               end
@@ -44,5 +34,38 @@ nodes.step(100, 30) do |nodes_number|
     end
   end
 end
+
+puts 'Start!'
+
+nodes               = [30, 50, 100, 200]
+default_delta       = [1, 2, 5]
+max_tries           = [3, 8]
+percentage_miss     = [0, 10, 30]
+percentage_faults   = [0, 10, 30]
+probability_to_fail = [0, 10, 30]
+bandwidth           = [50, 150, 300]
+latency             = [50, 200, 400]
+
+puts 'Early'
+
+execute('early', nodes, default_delta, max_tries, percentage_miss, percentage_faults, probability_to_fail, bandwidth, latency)
+
+puts 'Ring'
+
+if !(nodes > 50 && default_delta < 2)
+  execute('ring', nodes, default_delta, max_tries, percentage_miss, percentage_faults, probability_to_fail, bandwidth, latency)
+end
+
+puts 'Old Ring'
+
+execute('old_ring', nodes, default_delta, max_tries, percentage_miss, percentage_faults, probability_to_fail, bandwidth, latency)
+
+puts 'Centralized'
+
+execute('centralized', nodes, default_delta, max_tries, percentage_miss, percentage_faults, probability_to_fail, bandwidth, latency)
+
+puts 'Gossip'
+
+execute('gossip', nodes, default_delta, max_tries, percentage_miss, percentage_faults, probability_to_fail, bandwidth, latency)
 
 puts 'End.'
